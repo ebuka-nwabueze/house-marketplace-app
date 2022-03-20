2cadbf5ef80245c83b4881665b32e6d688a61a53
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-03-20 16:42:38 +0100

    Listing page updated to handle inputs of the field before processing to db

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ In the project directory, you can run:
 
 ### `npm start`
 
+create API Key for makeing request for the geo code API
+
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,9 +1,82 @@
-function Offers() {
-    return (
-        <div>
-            <h1>Offers</h1>
-        </div>
-    )
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import ListingItem from "../components/ListingItem";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase.config";
+
+function Offer() {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+
+  useEffect(() => {
+    const getListing = async () => {
+      try {
+        // get a query reference
+        const q = query(
+          collection(db, "listings"),
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+        // create snapshot for the database
+        const querySnapshot = await getDocs(q);
+        // saving the returned list
+        const fetchedList = [];
+        querySnapshot.forEach((doc) => {
+          return fetchedList.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        // update state hook with list
+        setListing(fetchedList);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Unable to fetch listings");
+      }
+    };
+
+    getListing();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listing && listing.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listing.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p> No offers available</p>
+      )}
+    </div>
+  );
 }
 
-export default Offers
+export default Offer;
